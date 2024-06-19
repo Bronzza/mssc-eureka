2bfe208d9765708923bb95f11cb0a15c27c0f073
Serhii Repinetskyi <repinetskiy@gmail.com>
2024-06-19 15:52:57 +0700

    Initial commit

---
+++ src/test/java/com/eureka/mssc_eureka/MsscEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.mssc_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
